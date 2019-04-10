c51aa812a4c7d4c760a730c3f61d73dbc70e6fec
wain77 <samwain@gmail.com>
2019-04-10 13:31:03 +0100

    Add project files.

---
+++ csharptesting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows;
+
+namespace csharptesting
+{
+    class Program
+    {
+        static void DrawPyramid(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write("  ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main()
+        {
+            DrawPyramid(5);
+        } 
+    }
+}
